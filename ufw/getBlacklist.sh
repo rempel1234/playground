949230812f6bee62d1b3ae462fa515aec09fec1b
rempel1234 <135683081+rempel1234@users.noreply.github.com>
2024-04-12 10:16:08 -0700

    Update getBlacklist.sh

--- ufw/getBlacklist.sh
+++ ufw/getBlacklist.sh
@@ -6,10 +6,10 @@
 curl -G https://api.abuseipdb.com/api/v2/blacklist \
   -d confidenceMinimum=50 \
   -H "Key: <API_KEY>" \
-  -H "Accept: text/plain" | sort > /path/to/blacklist
+  -H "Accept: text/plain" | sort > /tmp/blacklist
 
 # block every ip in list
 
-/usr/bin/blacklist
+/usr/local/bin/blacklist.sh
 
 # Shamelessly inspired by https://gist.github.com/MalteKiefer/407849891195a542dfa97329510aa387
