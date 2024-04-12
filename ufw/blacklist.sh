d72cc8b0f23fb2b636dc49180cf48cc2503c2de0
rempel1234 <135683081+rempel1234@users.noreply.github.com>
2024-04-12 11:10:46 -0700

    Update blacklist.sh

--- ufw/blacklist.sh
+++ ufw/blacklist.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+# Block both ways
 while read line; 
 do 
-	/usr/sbin/ufw prepend deny from $line to any; 
+	/usr/sbin/ufw prepend deny from $line to any;
+	/usr/sbin/ufw prepend deny from any to $line; 
 done < /tmp/blacklist.txt
