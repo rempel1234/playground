89c23e1d96bc5a26ea241fbf3c8a2bb5e098a5d9
rempel1234 <135683081+rempel1234@users.noreply.github.com>
2024-04-12 15:56:52 -0700

    Update initialize-blacklist.sh

--- iptables/initialize-blacklist.sh
+++ iptables/initialize-blacklist.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ipset create BlockAddress hash:ip hashsize 1024
+
+# intialize/allocate the blacklist
+ipset create IP_IPSUM hash:ip hashsize 1024
+
+iptables -t raw -A PREROUTING -m set --match-set IP_IPSUM src -j LOG --log-prefix "IPSUM:DROP:" --log-level 6
+iptables -t raw -A PREROUTING -m set --match-set IP_IPSUM src -j DROP
+iptables -t raw -A PREROUTING -m set --match-set IP_IPSUM dest -j LOG --log-prefix "IPSUM:DROP:" --log-level 6
+iptables -t raw -A PREROUTING -m set --match-set IP_IPSUM dest -j DROP
