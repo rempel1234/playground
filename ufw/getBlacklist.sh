99d784a913a5a201a046d0e42fe345a5493f697d
rempel1234 <135683081+rempel1234@users.noreply.github.com>
2024-04-12 10:09:06 -0700

    Create getBlacklist.sh

---
+++ ufw/getBlacklist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Place in /etc/cron.daily/getBlacklist
+
+# get latest black list from abuseIPDB
+
+curl -G https://api.abuseipdb.com/api/v2/blacklist \
+  -d confidenceMinimum=50 \
+  -H "Key: <API_KEY>" \
+  -H "Accept: text/plain" | sort > /path/to/blacklist
+
+# block every ip in list
+
+/usr/bin/blacklist
+
+# Shamelessly inspired by https://gist.github.com/MalteKiefer/407849891195a542dfa97329510aa387
