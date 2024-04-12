02a987ac89525a6698b9d77069c3b89af0e4ccfb
rempel1234 <135683081+rempel1234@users.noreply.github.com>
2024-04-12 15:33:32 -0700

    Update update-blacklist.sh

--- iptables/update-blacklist.sh
+++ iptables/update-blacklist.sh
@@ -2,7 +2,9 @@
 ipsumfile=/tmp/ipsum.ip
 
 /usr/bin/curl https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt -o $ipsumfile
-for d in www.facebook.com m.facebook.com facebook.com; do
-    dig +short "$d" >> "$fbookfile"
-done
-sort -n -u "$fbookfile" -o "$fbookfile"
+
+ipset flush IP_IPSUM
+
+while read ip; do
+    ipset add IP_IPSUM "$ip"
+done < /tmp/ipsum.ip
