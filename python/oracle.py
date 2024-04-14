5e3e4835852863da06f707c2bc1149316804a8bf
rempel1234 <135683081+rempel1234@users.noreply.github.com>
2024-04-14 15:58:03 -0700

    Create oracle.py

---
+++ python/oracle.py
@@ -0,0 +1,4 @@
+# To identify the lowest cost VM that meets the specs in a given city... 
+# https://www.oracle.com/cloud/public-cloud-regions/
+# https://www.oracle.com/cloud/price-list/#pricing-compute
+# https://docs.oracle.com/en-us/iaas/Content/Compute/References/computeshapes.htm
