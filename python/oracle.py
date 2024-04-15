2bf320e857bdb5091474a8f123bf590566c40bfd
rempel1234 <135683081+rempel1234@users.noreply.github.com>
2024-04-14 20:39:35 -0700

    Update oracle.py

--- python/oracle.py
+++ python/oracle.py
@@ -17,17 +17,44 @@ html = oracle_locations_response.text
 # parse the HTML
 soup = BeautifulSoup(html, "html.parser")
 
-df_oracle_zones = pd.DataFrame(columns=['region', 'zone'])
-
+df_oracle_zones = pd.DataFrame(columns=['country', 'zone','city'])
 for zone in soup.findAll('td'):
   if 'geo-data' in str(zone):
-    # the region/zone identifier
-    zone['geo-data']
     # the country/ordinal direction/city
     zone.contents[0]
     # the city
-    re.findall('\(([^)]+)', zone.contents[0])[0]
+    city = re.findall('\(([^)]+)', zone.contents[0])
+    if len(city) > 0:
+      city = re.findall('\(([^)]+)', zone.contents[0])[0]
+    else:
+      city = 'Singapore'
+    country = zone.contents[0].split('(')
+    if len(country) > 0:
+      country = country[0]
+    else:
+      country = ''
+    country = zone.contents[0].split(' ')
+    if len(country) > 0:
+      if country[0] != 'South':
+        # avoid cardinality for the country
+        country = country[0]
+      else:
+        # until there's a north/west/east africa or north/west/east korea region
+        # let's get rid of cardinality
+        country = country[1]
+    zone = zone['geo-data']
+    df_temp = pd.DataFrame(columns=['country', 'zone','city'])
+    df_temp.loc[0,'country'] = country
+    df_temp.loc[0,'zone'] = zone
+    df_temp.loc[0,'city'] = city
+    df_oracle_zones = pd.concat([df_oracle_zones, df_temp], ignore_index=True)
+    df_oracle_zones.reset_index()
 
+# get rid of things we can't use anyway...
+df_oracle_zones = df_oracle_zones[df_oracle_zones['country']!='EU']
+
+
+df_oracle_zones = pd.DataFrame(columns=['region', 'zone'])
 for zone in soup.findAll('th'):
   if 'zone' in str(zone):
     zone_str=zone.contents[0]
